9da0d2cb10ffb49facfecbdd033046874019eafb
Wilsonfb <150964778+Wilsonfb@users.noreply.github.com>
2024-11-14 19:47:08 -0500

    Vistas

--- vistasUsuarios.py
+++ vistasUsuarios.py
@@ -5,45 +5,53 @@ from flask import request
 usuarios_schema = UsuariosSchema()
 
 class VistaUsuarios(Resource):
+
     def get(self):
         return [usuarios_schema.dump(Usuarios) for Usuarios in Usuarios.query.all()]
     
-    def post(self, Cargo, Nombres, Apellidos, Edad, Calle, Telefono, Correo, Genero, Identidad, Numero_Identidad):
+    def post(self, nombres, apellidos, correo, contraseña, telefono, direccion, cargo_id):
         nuevo_usuario_data = request.get_json()
-        Cargo = nuevo_usuario_data.get("Cargo")
-        Nombres = nuevo_usuario_data.get("Nombres")
-        Apellidos = nuevo_usuario_data.get("Apellidos")
-        Edad = nuevo_usuario_data.get("Edad")
-        Calle = nuevo_usuario_data.get("Calle")
-        Telefono = nuevo_usuario_data.get("Telefono")
-        Correo = nuevo_usuario_data.get("Correo")
-        Genero = nuevo_usuario_data.get("Genero")
-        Identidad = nuevo_usuario_data.get("Identidad")
-        Numero_Identidad = nuevo_usuario_data.get("Numero_Identidad")
-
-        if Cargo not in ["gerente", "empleado"]:
-            return {"ADVERTENCIA": "El cargo debe ser 'gerente' o 'empleado'."}, 400
-        if not isinstance(Nombres, str):
-            return {"ADVERTENCIA": "Los dos primeros nombres del usuario son requerido."}, 400
-        if not isinstance(Apellidos, str):
-            return {"ADVERTENCIA": "Los dos priemros apellidos del usuario son requerido."}, 400
-        if not isinstance(Edad, int):
-            return {"ADVERTENCIA": "La edad debe ser mayor de 18."}, 400
-        if not isinstance(Calle, str):
-            return {"ADVERTENCIA": "Debe ser la casa o dirrecion donde vive usted."},400
-        if not isinstance(Telefono, int):
-            return {"ADVERTENCIA": "Tiene que ser su telefono de contacto."},400
-        if not isinstance(Correo, str):
-            return {"ADVERTENCIA": "Su correo electronico personal."},400
-        if Genero not in ["Hombre", "Mujer"]:
-            return {"ADVERTENCIA": "El genero es obligatorio y es 'hombre' o 'mujer'."}, 400
-        if Identidad not in ["Cedula", "Cedula Extranger"]:
-            return {"ADVERTENCIA": "Digite que tipo es su identificacion si es 'Cedula' o 'Cedula Extrangera'."}, 400        
-        if not isinstance(Numero_Identidad, int):
-            return {"ADVERTENCIA": "Digite el numero de su identidad obligatorio."},400
-
-        nuevo_usuario = Usuarios(Cargo = Cargo, Nombres = Nombres, Apellidos = Apellidos, Edad = Edad, Calle = Calle, Telefono = Telefono, Correo = Correo, Genero = Genero, Identidad = Identidad, Numero_Identidad = Numero_Identidad)
+        cargo_id = nuevo_usuario_data.get("cargo_id")
+        nombres = nuevo_usuario_data.get("nombres")
+        apellidos = nuevo_usuario_data.get("apellidos")
+        correo = nuevo_usuario_data.get("correo")
+        direccion = nuevo_usuario_data.get("dirrecion")
+        telefono = nuevo_usuario_data.get("telefono")
+        contraseña = nuevo_usuario_data.get("contraseña")
+        if not isinstance(nombres, str):
+            return {"ADVERTENCIA": "Los dos primeros nombres del usuario son requerido."}, 404
+        if not isinstance(apellidos, str):
+            return {"ADVERTENCIA": "Los dos priemros apellidos del usuario son requerido."}, 404
+        if not isinstance(correo, str):
+            return {"ADVERTENCIA": "Su correo electronico personal."},404
+        if not isinstance(contraseña, int):
+            return {"ADVERTENCIA": "Digite el numero de su identidad obligatorio."},404
+        if not isinstance(telefono, int):
+            return {"ADVERTENCIA": "Tiene que ser su telefono de contacto."},404
+        if not isinstance(direccion, str):
+            return {"ADVERTENCIAS": "Digite la dirrecion de su residencia actual."},404
+        if cargo_id not in ["gerente", "empleado"]:
+            return {"ADVERTENCIA": "El cargo_id debe ser 'gerente' o 'empleado'."}, 404
+        nuevo_usuario = Usuarios(nombres = nombres, apellidos = apellidos, correo = correo, contraseña = contraseña, telefono = telefono, direccion = direccion, cargo_id = cargo_id)
         db.session.add(nuevo_usuario)
         db.session.commit()
-
-        return usuarios_schema.dump(nuevo_usuario), 201
+        return usuarios_schema.dump(nuevo_usuario), 404
+    
+    def put(self, usuario_id):
+           usuario = Usuarios.query.get_or_404(usuario_id)
+           usuario_data = request.get_json()
+           usuario.nombres = usuario_data.get("nombres", usuario.nombres)
+           usuario.apellidos = usuario_data.get("apellidos", usuario.apellidos)
+           usuario.correo = usuario_data.get("correo", usuario.correo)
+           usuario.contraseña = usuario_data.get("contraseña", usuario.contraseña)
+           usuario.telefono = usuario_data.get("telefono", usuario.telefono)
+           usuario.direccion = usuario_data.get("direccion", usuario.direccion)
+           usuario.cargo_id = usuario_data.get("cargo_id", usuario.cargo_id)
+           db.session.commit()
+           return usuarios_schema.dump(usuario), 404
+    
+    def delete(self, usuario_id):
+        usuario = Usuarios.query.get_or_404(usuario_id)
+        db.session.delete(usuario)
+        db.session.commit()
+        return {"MENSAJE": "Usuario eliminado."}, 404  
